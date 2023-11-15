90fe87384fd4d0c1c562af3b77fc8ec80a57d6e0
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-11-15 00:29:24 -0800

    try

---
+++ main.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: nr of arguments
+ * @argv: list of arguments
+ * Return: 0 or 1
+ */
+int main(int argc, char argv[])
+{
+	size_t buff = 1024, count = 0;
+	ssize_t line;
+	stack_t *head = NULL;
+	char  *str = NULL, *ar[2], *tmp;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		line = getline(&str, &buff, file);
+		if (line == -1)
+			break;
+		count++;
+		ar[0] = strtok(str, "\n ");
+		if (ar[0] == NULL)
+			get_nop(&head, count);
+		else if (strcmp("push", ar[0]) == 0)
+		{
+			tmp = strtok(NULL, "\n ");
+
+			get_push(&head, count, tmp);
+		}
+		else if (ar[0] != NULL && ar[0][0] != '#')
+		{
+			operator_function = go(ar[0], count, &head);
+
+			if (operator_function == NULL && count == 0)
+			{
+				fprintf(stderr, "L%ld: unknown instruction %s\n",
+					count, ar[0]), exit(EXIT_FAILURE);
+			}
+		operator_function(&head, count);
+		}
+	}
+	fclose(file), free(str), get_free(head);
+	return (0);
+}
